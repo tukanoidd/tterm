4e64be64bdeef3bcc0a7916cf34c56eb431fa632
Vitalii Lukyanov <tukanoid@outlook.com>
2026-06-23 20:50:28 +0200

    simple home manager module

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    home-manager.url = "github:nix-community/home-manager";
 
     iced-comet = {
       # 0.14.0 tag
@@ -23,10 +24,14 @@
     iced-comet,
     ...
   }:
-    parts.lib.mkFlake {inherit inputs;} {
+    parts.lib.mkFlake {inherit inputs;} ({self, ...}: let
+      outPkg = pkgs: self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    in {
       systems = ["x86_64-linux"];
       imports = [
         nci.flakeModule
+        inputs.home-manager.flakeModules.home-manager
+
         ./crates.nix
       ];
       perSystem = {
@@ -56,5 +61,47 @@
         });
         packages.default = ttermOutputs.packages.release;
       };
-    };
+
+      flake.homeModules.default = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }: let
+        cfg = config.programs.tterm;
+        pkg = outPkg pkgs;
+      in
+        with lib; {
+          options = {
+            programs.tterm = {
+              enable = mkEnableOption "tterm";
+              package = mkOption {
+                type = types.package;
+                default = pkg;
+                description = "tterm package derivation";
+              };
+              configFilePath = mkOption {
+                type = types.str;
+                default = ".config/tterm/config.ron";
+                description = "Path to config file in $HOME";
+              };
+              configFile = mkOption {
+                type = types.nullOr types.path;
+                default = null;
+                description = "Path to .ron config file to link to the 'configFilePath' location";
+              };
+            };
+          };
+
+          config = mkIf cfg.enable {
+            home = {
+              packages = [cfg.package];
+
+              file.${cfg.configFilePath} = mkIf (cfg.configFile != null) {
+                source = cfg.configFile;
+              };
+            };
+          };
+        };
+    });
 }
